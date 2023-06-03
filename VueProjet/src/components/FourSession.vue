b6a5df6a9a4f0718de2d99eef4a35f76afb97566
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-06-03 23:15:31 +0100

    four session

---
+++ VueProjet/src/components/FourSession.vue
@@ -0,0 +1,33 @@
+<script>
+import Devinette from './Devinette.vue'
+import EtoileAge from './EtoileAge.vue'
+import VisuelImage from './VisuelImage.vue'
+
+export default {
+  components: {
+    Devinette,
+    VisuelImage,
+    EtoileAge
+  }
+}
+</script>
+<template>
+  <div class="session">
+    <div class="foursession">
+      <Devinette />
+      <div class="visuelimage_age">
+        <EtoileAge />
+        <VisuelImage />
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.foursession {
+  width: 65%;
+}
+.foursession .visuelimage_age {
+  display: flex;
+  margin-top: 30px;
+}
+</style>
