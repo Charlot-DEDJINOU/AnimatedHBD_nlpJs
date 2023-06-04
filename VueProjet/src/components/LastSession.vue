f49ab32ddb698c49584ef384617daca37e315cc6
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-06-04 16:28:16 +0100

    new session

---
+++ VueProjet/src/components/LastSession.vue
@@ -0,0 +1,43 @@
+<script>
+import Devinette from './Devinette.vue';
+import VisuelLivre from './VisuelLivre.vue'
+import { ref } from 'vue'
+
+export default {
+    components : {
+        Devinette ,
+        VisuelLivre
+    } ,
+    setup() {
+    const showSession = ref(false)
+
+    const nextSession = (payload) => {
+      if(payload.message){
+        showSession.value = true
+      }
+    }
+    return {
+      nextSession ,
+      showSession
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="session">
+        <div class="LastSession">
+            <Devinette @backAnswer="nextSession" devinette="Tout le monde en a une, mais personne ne peut la perdre , Qui suis je ?" numAnswer="3"/>
+            <VisuelLivre v-if="showSession"/>
+        </div>
+    </div>
+</template>
+<style>
+.LastSession 
+{
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
