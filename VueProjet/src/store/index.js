da66d3883bea6f76e96d7c416c828ca68ee80c71
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-06-05 23:48:55 +0100

    vuex

--- VueProjet/src/store/index.js
+++ VueProjet/src/store/index.js
@@ -1,9 +1,30 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
+  state: {
+    uniColor: "#16c953",
+    secondColor: "#FFDAA2"
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    setUniColor(state, color) {
+      state.uniColor = color
+    },
+    setSecondColor(state, color) {
+      state.secondColor = color
+    }
+  },
+  actions: {
+    Color({ commit }, color) {
+      commit('setUniColor', color)
+
+      let newColor = "#FFDAA2"
+      if (['#c0da17', 'pink' , '#16c953'].includes(color)) {
+        newColor = "#8c49b2"
+      }
+
+      commit('setSecondColor', newColor)
+    }
+  },
   modules: {}
 })
