794d8de18d75981fbdecccfb0707872851980304
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-14 11:50:06 +0100

    Login function

---
+++ Backend/src/controllers/userController.js
@@ -0,0 +1,12 @@
+exports.login = async (req, res) => {
+  try {
+      const { password } = req.body;
+      if (process.env.USER_PASSWORD == password) {
+          res.status(200).json({ message: 'Authentication successful' });
+          return;
+      }
+      res.status(401).json({ message: 'Authentication failed' });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+};
