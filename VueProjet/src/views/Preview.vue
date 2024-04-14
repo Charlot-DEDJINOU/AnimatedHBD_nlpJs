81d905612e7a0a7428c75ef4ca3d75143b329065
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-14 12:46:24 +0100

    login

--- VueProjet/src/views/Preview.vue
+++ VueProjet/src/views/Preview.vue
@@ -2,6 +2,7 @@
 import HomeView from './HomeView.vue'
 import { ref } from 'vue'
 import { onMounted } from 'vue'
+import { login } from '../services/authService'
 
 export default {
   components: {
@@ -10,13 +11,16 @@ export default {
   setup() {
     const reponse = ref('')
     const show = ref(false)
+    const showError = ref(false)
     const count = ref(0)
     var timeOut = ''
 
-    const submitCode = () => {
-      if (reponse.value === 'Nerys') show.value = true
+    const submitCode = async () => {
+      const response = await login({ password : reponse.value })
+      if (response.status === 200 && response.message === "Authentication successful") show.value = true
       else {
         reponse.value = ''
+        showError.value = true
         document.getElementsByClassName('code')[0].classList.add('danger')
       }
     }
@@ -35,27 +39,31 @@ export default {
       reponse,
       submitCode,
       show,
-      count
+      count,
+      showError
     }
   }
 }
 </script>
 <template>
-  <HomeView v-if="show" />
-  <div class="preview" v-if="!show">
-    <img src="../assets/chiffre1.png" id="image" v-if="count == 0" />
-    <img src="../assets/chiffre2.png" id="image" v-if="count == 1" />
-    <img src="../assets/chiffre3.png" id="image" v-if="count == 2" />
-    <img src="../assets/chiffre4.png" id="image" v-if="count == 3" />
-    <img src="../assets/chiffre5.png" id="image" v-if="count == 4" />
-    <img src="../assets/chiffre6.png" id="image" v-if="count == 5" />
-    <img src="../assets/chiffre7.png" id="image" v-if="count == 6" />
-    <img src="../assets/chiffre8.png" id="image" v-if="count == 7" />
-    <img src="../assets/chiffre9.png" id="image" v-if="count == 8" />
-    <form class="preview_form" @submit.prevent="submitCode">
-      <input type="password" placeholder="code secret" v-model="reponse" class="code" />
-      <button>Envoyer</button>
-    </form>
+  <div>
+    <HomeView v-if="show" />
+    <div class="preview" v-if="!show">
+      <img src="../assets/chiffre1.png" id="image" v-if="count == 0" />
+      <img src="../assets/chiffre2.png" id="image" v-if="count == 1" />
+      <img src="../assets/chiffre3.png" id="image" v-if="count == 2" />
+      <img src="../assets/chiffre4.png" id="image" v-if="count == 3" />
+      <img src="../assets/chiffre5.png" id="image" v-if="count == 4" />
+      <img src="../assets/chiffre6.png" id="image" v-if="count == 5" />
+      <img src="../assets/chiffre7.png" id="image" v-if="count == 6" />
+      <img src="../assets/chiffre8.png" id="image" v-if="count == 7" />
+      <img src="../assets/chiffre9.png" id="image" v-if="count == 8" />
+      <form class="preview_form" @submit.prevent="submitCode">
+        <input type="password" placeholder="code secret" v-model="reponse" class="code" />
+        <div class="alert-danger" v-if="showError">Le code secret est incorrect</div>
+        <button>Envoyer</button>
+      </form>
+    </div>
   </div>
 </template>
 <style scoped>
@@ -74,7 +82,8 @@ export default {
 }
 .preview form {
   width: 300px;
-  height: 100px;
+  min-height: 100px;
+  height: auto;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -95,8 +104,17 @@ export default {
   color: white;
 }
 .danger {
-  box-shadow: 0px 0px 3px 1px red;
-  outline: red;
+  box-shadow: 0px 0px 3px 1px rgba(148, 68, 68, 0.744);
+  outline: rgba(148, 68, 68, 0.744);
   border: none;
+}
+.alert-danger {
+  background-color: rgba(148, 68, 68, 0.744);
+  border-radius: 5px;
+  height: 45px;
+  padding: 10px;
+  width: 100%;
+  margin: 10px 0px;
+  color : rgb(153, 14, 14);
 }
 </style>
