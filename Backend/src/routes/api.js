8f5d3f42ef511332d9d48660463d7e7f9f5f42ce
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-14 11:51:58 +0100

    app routes

---
+++ Backend/src/routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const devinetteController = require('../controllers/devinetteController');
+
+router.post('/login', userController.login);
+router.post('/preprocessing', devinetteController.preprocessing);
+
+module.exports = router;
