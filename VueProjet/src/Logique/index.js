2b6be5d7daecba4031f89191df54e3e96fe03eff
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-14 11:44:54 +0100

    remove logique nlp directory

--- VueProjet/src/Logique/index.js
+++
@@ -1,9 +0,0 @@
-import { main as nlpMain } from './Logique/nlp'
-
-export function predict(numDev, userAnswer) {
-  return new Promise((resolve) => {
-    nlpMain(userAnswer, numDev, (answer) => {
-      resolve({ message: answer })
-    })
-  })
-}
