78bdb7c0f4c196c25b428a0a9bff4be88e92fc2d
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-14 12:47:19 +0100

    app services

---
+++ VueProjet/src/services/devinetteService.js
@@ -0,0 +1,11 @@
+import api from './api';
+
+export const predict = async (data) => {
+    try {
+        const response = await api.post('/preprocessing', data);
+        const { success } = response.data;
+        return {message : success} ;
+    } catch (error) {
+        console.log(error.response.status) ;
+    }
+};
