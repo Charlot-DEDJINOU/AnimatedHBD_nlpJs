4be23f09db4a4e821bbe3adf922d2c121f62fc85
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-15 15:53:37 +0100

    login function

--- VueProjet/src/services/authService.js
+++ VueProjet/src/services/authService.js
@@ -1,11 +1,11 @@
-import api from './api';
+import api from './api'
 
 export const login = async (credentials) => {
-    try {
-        const response = await api.post('/login', credentials);
-        const { message } = response.data;
-        return { message, status : response.status } ;
-    } catch (error) {
-        return { status : error.response.status };
-    }
-};
+  try {
+    const response = await api.post('/login', credentials)
+    const { message } = response.data
+    return { message, status: response.status }
+  } catch (error) {
+    return { status: error.response.status }
+  }
+}
