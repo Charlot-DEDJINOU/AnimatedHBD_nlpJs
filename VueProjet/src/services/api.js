61c410686d2a8e76554e175bf11bb7ab33113120
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-15 15:53:54 +0100

    api url

--- VueProjet/src/services/api.js
+++ VueProjet/src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api', 
-});
+  baseURL: 'https://animatedhbd-nlpjsapi.onrender.com/api'
+})
 
-export default api;
+export default api
