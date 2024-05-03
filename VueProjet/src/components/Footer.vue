7b4a73157677efb9a159f904d89eee3c5559bf32
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-03 11:54:41 +0100

    Footer component

---
+++ VueProjet/src/components/Footer.vue
@@ -0,0 +1,75 @@
+<template>
+  <footer class="w-full bg-black flex items-center justify-center p-5">
+    <div class="container flex flex-col items-center">
+      <b class="text-center mb-3 text-3xl text-green">Happy Birth Day</b>
+      <p class="text-center">
+        L’être humain à le potentiel de devenir quelque chose d’autre - David Goggins
+      </p>
+      <div class="copyright my-4">
+        &copy; Copyright <strong><span>HBD </span></strong>All Rights Reserved
+      </div>
+      <div>
+        Created by
+        <a
+          href="https://charlot-dedjinou.vercel.app"
+          class="font-bold text-white text-decoration-none"
+        >
+          Charlot DEDJINOU</a
+        >
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style>
+.w-full {
+  width: 100%;
+}
+.bg-black {
+  background-color: #000000;
+}
+.flex {
+  display: flex;
+}
+.items-center {
+  align-items: center;
+}
+.justify-center {
+  justify-content: center;
+}
+.p-5 {
+  padding: 1.25rem;
+}
+.container {
+  margin-right: auto;
+  margin-left: auto;
+}
+.flex-col {
+  flex-direction: column;
+}
+.mb-3 {
+  margin-bottom: 0.75rem;
+}
+.text-center {
+  text-align: center;
+}
+.text-3xl {
+  font-size: 1.875rem;
+}
+.text-green {
+  color: #16c953;
+}
+.my-4 {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+.font-bold {
+  font-weight: 700;
+}
+.text-white {
+  color: white;
+}
+.text-decoration-none {
+  text-decoration: none;
+}
+</style>
--- VueProjet/src/views/Preview.vue
+++ VueProjet/src/views/Preview.vue
@@ -1,5 +1,6 @@
 <script>
 import HomeView from './HomeView.vue'
+import Footer from '../components/Footer.vue'
 import { ref } from 'vue'
 import { onMounted } from 'vue'
 import { login } from '../services/authService'
@@ -8,6 +9,7 @@ import Loading from '../components/icons/Loading.vue'
 export default {
   components: {
     HomeView,
+    Footer,
     Loading
   },
   setup() {
@@ -52,36 +54,38 @@ export default {
 }
 </script>
 <template>
-  <div>
-    <HomeView v-if="show" />
-    <div class="preview" v-if="!show">
-      <img src="../assets/chiffre1.png" id="image" v-if="count == 0" />
-      <img src="../assets/chiffre2.png" id="image" v-if="count == 1" />
-      <img src="../assets/chiffre3.png" id="image" v-if="count == 2" />
-      <img src="../assets/chiffre4.png" id="image" v-if="count == 3" />
-      <img src="../assets/chiffre5.png" id="image" v-if="count == 4" />
-      <img src="../assets/chiffre6.png" id="image" v-if="count == 5" />
-      <img src="../assets/chiffre7.png" id="image" v-if="count == 6" />
-      <img src="../assets/chiffre8.png" id="image" v-if="count == 7" />
-      <img src="../assets/chiffre9.png" id="image" v-if="count == 8" />
-      <form class="preview_form" @submit.prevent="submitCode">
-        <input type="password" placeholder="code secret" v-model="reponse" class="code" />
-        <div class="alert-danger" v-if="showError">Le code secret est incorrect</div>
-        <button>
-          <div v-if="loading">
-            <Loading />
-            <span>Loading...</span>
-          </div>
-          <span v-else>S'authentifier</span>
-        </button>
-      </form>
-    </div>
+  <div class="flex flex-col min-h-screen">
+    <main class="flex-grow">
+      <HomeView v-if="show" />
+      <div class="preview" v-if="!show">
+        <img src="../assets/chiffre1.png" id="image" v-if="count == 0" />
+        <img src="../assets/chiffre2.png" id="image" v-if="count == 1" />
+        <img src="../assets/chiffre3.png" id="image" v-if="count == 2" />
+        <img src="../assets/chiffre4.png" id="image" v-if="count == 3" />
+        <img src="../assets/chiffre5.png" id="image" v-if="count == 4" />
+        <img src="../assets/chiffre6.png" id="image" v-if="count == 5" />
+        <img src="../assets/chiffre7.png" id="image" v-if="count == 6" />
+        <img src="../assets/chiffre8.png" id="image" v-if="count == 7" />
+        <img src="../assets/chiffre9.png" id="image" v-if="count == 8" />
+        <form class="preview_form" @submit.prevent="submitCode">
+          <input type="password" placeholder="code secret" v-model="reponse" class="code" />
+          <div class="alert-danger" v-if="showError">Le code secret est incorrect</div>
+          <button>
+            <div v-if="loading">
+              <Loading />
+              <span>Loading...</span>
+            </div>
+            <span v-else>S'authentifier</span>
+          </button>
+        </form>
+      </div>
+    </main>
+    <Footer />
   </div>
 </template>
 <style scoped>
 .preview {
   width: 100%;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -131,5 +135,20 @@ export default {
   width: 100%;
   margin: 10px 0px;
   color: rgb(226, 97, 97);
+}
+.flex {
+  display: flex;
+}
+.flex-col {
+  flex-direction: column;
+}
+.min-h-screen {
+  min-height: 100vh;
+}
+.flex-grow {
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
